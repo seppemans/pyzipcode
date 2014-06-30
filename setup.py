b6e6611e61dc135e8c9d7d50eebdd1b4ccc1c470
seppemans <sep@seplog.net>
2014-06-30 10:38:21 -0700

    Adjust setup.py to correctly install with the zipcodes.db file

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ else:
 setup(name='pyzipcode',
       version=version,
       description="query zip codes and location data",
-      long_description=open("README.txt").read() + '\n\n' + open('CHANGES.txt').read(),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='zip code distance',
       author='Nathan Van Gheem',
@@ -21,10 +20,15 @@ setup(name='pyzipcode',
       url='',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       entry_points="""
       # -*- Entry points: -*-
       """,
+
+      # Include additional files into the package
+      include_package_data=True,
+      package_data = {
+          '': ['zipcodes.db'],
+      }
       )
